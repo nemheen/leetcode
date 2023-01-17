1a5857b74978b920ddadcba9a59ae38cced56654
nemheen <40517906+nemheen@users.noreply.github.com>
2023-01-17 05:00:07 +0000

    [LeetCode Sync] Runtime - 16 ms (6.01%), Memory - 14.9 MB (99.97%)

--- my-folder/0021-merge-two-sorted-lists/solution.cpp
+++ my-folder/0021-merge-two-sorted-lists/solution.cpp
@@ -1,46 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-      
         if(list1 == NULL) return list2;
         if(list2 == NULL) return list1;
-        struct ListNode* mlist = list1;
-        if(list2->val < list1->val){
+        
+        ListNode * mlist = list1;
+        if(list1 -> val > list2 -> val)
+        {
             mlist = list2;
-            list2 = list2->next;
+            list2 = list2 -> next;
         }
         else
         {
-            list1 = list1->next;
+            list1 = list1 -> next;
         }
-        struct ListNode* temp = mlist;
-        while(list1 != NULL && list2 != NULL) 
-        {   if(list2->val < list1->val){
-                mlist->next = list2;
-                list2 = list2->next;
+        ListNode *temp = mlist;
+
+        while(list1 &&  list2)
+        {
+            if(list1 -> val < list2 -> val){
+                temp->next = list1;
+                list1 = list1 -> next;
             }
-            else {
-                mlist->next = list1;
-                list1 = list1->next;
+            else{
+                temp->next = list2;
+                list2 = list2 -> next;
             }
-            mlist = mlist->next;
+            temp = temp -> next;        
         }
+		
         if(!list1)
-            mlist->next = list2;
-        else 
-            mlist->next = list1;
-
-        return temp;
-
+            temp -> next = list2;
+        else
+            temp -> next = list1;
+            
+        return mlist;
     }
 };
