8fb7350999957d42382925601b08332b19a77a38
nemheen <40517906+nemheen@users.noreply.github.com>
2023-05-08 06:42:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

---
+++ my-folder/0062-unique-paths/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> row(n, 1);
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j<n; j++) {
+                row[j] += row[j - 1];
+            }
+        }
+        
+        return row[n-1];
+    }
+};
+
+  
