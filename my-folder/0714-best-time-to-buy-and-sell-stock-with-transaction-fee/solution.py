0f1a81dc91a27ceacdcbf5e17d8e46554af43969
nemheen <40517906+nemheen@users.noreply.github.com>
2023-05-16 13:41:21 +0000

    [LeetCode Sync] Runtime - 912 ms (18.96%), Memory - 25.6 MB (38.67%)

---
+++ my-folder/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+
+        if not prices:
+            return 0
+        
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n)]
+        
+        # Base case initialization
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee)  # selling
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])  # buying
+        return dp[n-1][0]
+
+
