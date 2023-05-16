41979c631769dfb83c1b971e3ea950d9b4908fe9
nemheen <40517906+nemheen@users.noreply.github.com>
2023-05-16 11:09:12 +0000

    [LeetCode Sync] Runtime - 1212 ms (5.23%), Memory - 18.6 MB (41.11%)

---
+++ my-folder/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+   
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        t = s[::-1]
+        dp = [[0 for j in range(n+1)] for i in range(n+1)]
+        for i in range(n-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if t[i] == s[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+        return dp[0][0]
+    def minInsertions(self, s: str) -> int:
+        ans = self.longestPalindromeSubseq(s)
+        return len(s) - ans
+
