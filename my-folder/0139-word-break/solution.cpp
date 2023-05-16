022d374a1d68e40c32c51c69dbd56d9293e10414
nemheen <40517906+nemheen@users.noreply.github.com>
2023-05-16 13:19:54 +0000

    [LeetCode Sync] Runtime - 13 ms (45.12%), Memory - 13.2 MB (60.26%)

--- my-folder/0139-word-break/solution.cpp
+++ my-folder/0139-word-break/solution.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& dict) {
-        unordered_set<string> bag(dict.begin(), dict.end());
-        unordered_map<int, bool> cache;
-		
-        // Define a recursive function to solve the problem
-        function<bool(int)> go = [&](auto start) {
-            // Base case: if we have reached the end of the string, return true
-            if (start == s.size()) return true;
-            // Check if we have already solved this subproblem
-            if (cache.count(start)) return cache[start];
-            
-            // Otherwise, try all possible ways to split the string from the current position
-            bool is_possible = false;
-            string builder = "";
-            for (int i = start; i < s.size(); i++) {
-                builder += s[i];
-                if (bag.count(builder)) is_possible |= go(i + 1);
-            }
-            // Memoize the result of this subproblem and return it
-            return cache[start] = is_possible;
-        };
-		
-        // Solve the problem starting from the beginning of the string
-        return go(0);
+       int n = s.length();
+       unordered_set<string> word(dict.begin(), dict.end());
+
+       vector<bool> dp(n+1, false);
+       dp[0] = true;
+
+       for(int i=0; i<=n; i++) {
+           for(int j=0; j<i; j++) {
+               if(dp[j] && word.count(s.substr(j, i-j))) {
+                   dp[i] = true;
+                 
+               }
+           }
+       }
+        return dp[n];
+
+
     }
 };
