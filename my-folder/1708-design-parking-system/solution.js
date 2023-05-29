ff31b58d5fd08ce49841f767eac8a53f0a47e96b
nemheen <40517906+nemheen@users.noreply.github.com>
2023-05-29 04:41:21 +0000

    [LeetCode Sync] Runtime - 134 ms (5.14%), Memory - 49.9 MB (100.00%)

---
+++ my-folder/1708-design-parking-system/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+    this.spots = [big, medium, small];
+    
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    if (this.spots[carType - 1] > 0) {
+        this.spots[carType-1]--;
+        return true;
+    }
+    else { return false; }
+    
+};
+
+/** 
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
