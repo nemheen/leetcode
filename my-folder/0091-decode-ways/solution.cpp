86c0f299c682b7c0802ef1507bfbeb811e0594ae
nemheen <40517906+nemheen@users.noreply.github.com>
2023-06-05 06:00:47 +0000

    [LeetCode Sync] Runtime - 5 ms (13.27%), Memory - 6.3 MB (100.00%)

---
+++ my-folder/0091-decode-ways/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.empty() || s[0] == '0')
+            return 0;
+        
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            int singleDigit = stoi(s.substr(i - 1, 1));
+            int doubleDigits = stoi(s.substr(i - 2, 2));
+
+            if (singleDigit >= 1 && singleDigit <= 9)
+                dp[i] += dp[i - 1];
+            
+            if (doubleDigits >= 10 && doubleDigits <= 26)
+                dp[i] += dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+};
+
