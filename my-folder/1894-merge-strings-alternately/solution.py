47658504cedf451c9cc888955d3a990f885aa892
nemheen <40517906+nemheen@users.noreply.github.com>
2023-06-15 07:14:55 +0000

    [LeetCode Sync] Runtime - 40 ms (20.84%), Memory - 16.2 MB (99.80%)

---
+++ my-folder/1894-merge-strings-alternately/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        n = len(word1)
+        m = len(word2)
+
+        minn = min(m, n)
+
+        ans = ""
+        i = 0
+        for i in range(minn):
+           ans += word1[i]
+           ans += word2[i]
+        
+        if n > m:
+            ans += word1[minn:]
+        else: ans+= word2[minn:]
+
+        return ans
+            
