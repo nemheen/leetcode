1908f0e6a5f6516d7a30c35c44c89c7270acf0d5
nemheen <40517906+nemheen@users.noreply.github.com>
2023-06-24 08:22:07 +0000

    [LeetCode Sync] Runtime - 110 ms (6.72%), Memory - 21.2 MB (45.68%)

---
+++ my-folder/0347-top-k-frequent-elements/solution.py
@@ -0,0 +1,14 @@
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        c=Counter(nums)
+        c=[(-v,k) for k,v in c.items()]
+        heapq.heapify(c)
+        output = []
+        for i in range(k):
+            item = heapq.heappop(c)
+            output.append(item[1])
+
+        return output
+
+
