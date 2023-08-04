deeb35e04fdea707f80f2841da6f187b97287383
nemheen <40517906+nemheen@users.noreply.github.com>
2023-08-04 12:33:27 +0000

    [LeetCode Sync] Runtime - 50 ms (30.23%), Memory - 22.2 MB (5.27%)

--- my-folder/0125-valid-palindrome/solution.py
+++ my-folder/0125-valid-palindrome/solution.py
@@ -1,5 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        a = [x.lower() for x in s if x.isalnum()]
-        return a == a[::-1]
+        s = s.replace(" ", "")
+        punctuations = '''!()-[]{};:'`"\,<>./?@#$%^&*_~'''
+        result = "".join([c.lower() for c in s if c not in punctuations])
+
+        l, r = 0, len(result) - 1
+
+        while l < r:
+            if result[l] != result[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+        
 
