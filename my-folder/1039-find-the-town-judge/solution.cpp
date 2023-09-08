f8a2ecc8952ba6888ce537cf7c42ebd4110c368a
nemheen <40517906+nemheen@users.noreply.github.com>
2023-09-08 14:51:31 +0000

    [LeetCode Sync] Runtime - 128 ms (45.19%), Memory - 60.8 MB (99.89%)

--- my-folder/1039-find-the-town-judge/solution.cpp
+++ my-folder/1039-find-the-town-judge/solution.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& t) {
-       vector<int>tcount(n+1, 0);
+       vector<int>res(n+1, 0);
+       if(n==1) return 1;
 
-       if(t.empty() && n==1) return 1;
        for(int i=0; i<t.size(); i++) {
-           tcount[t[i][0]]--;
-           tcount[t[i][1]]++;
-
+           res[t[i][0]]--;
+           res[t[i][1]]++;
        }
-       for(int i=0; i<n+1; i++) {
-           if(tcount[i]==n-1) return i;
+       for(int i=0; i<=n; i++) {
+           if (res[i] == n-1) return i;
        }
 
        return -1;
