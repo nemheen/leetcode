39a47ba2ff7c7280c291e9be1e15e382c253e7a2
nemheen <40517906+nemheen@users.noreply.github.com>
2023-09-13 05:49:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.3 MB (100.00%)

--- my-folder/0020-valid-parentheses/solution.cpp
+++ my-folder/0020-valid-parentheses/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int i, n, j=0;
-        stack<char> stack;
-        n = s.size();
-        if(n == 0) return true;
-        if(n%2 != 0) return false;
-        for(i=0; i<n; i++) {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
-                stack.push(s[i]);
-            }
-            else if ( (s[i] == ')' && !stack.empty() && stack.top() == '(') ||
-                        (s[i] == '}' && !stack.empty() && stack.top() == '{') ||
-                        (s[i] == ']' && !stack.empty() && stack.top() == '[')
-                      ){ stack.pop(); }
-            else return false;
-            }
-        if(stack.empty()) return true;
-        return false;  
+        stack<char> st;
+        for(char c: s) {
+          if(c == '(' || c == '{' || c == '[') {
+            st.push(c);
+          }
+          else if(st.empty() || (c == ')' && st.top() != '(') || (c == '}' && st.top() != '{') 
+          || (c == ']' && st.top() != '[')) {
+                    return false; 
+                }
+          else {
+            st.pop();
+          }
+          
+        }
+        return st.empty();
     }
 };
