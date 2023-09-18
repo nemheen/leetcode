5eb7492d66c5b7f1e31bd4d98f484eac7363bd84
nemheen <40517906+nemheen@users.noreply.github.com>
2023-09-18 09:15:00 +0000

    [LeetCode Sync] Runtime - 66 ms (86.26%), Memory - 44.5 MB (99.81%)

--- my-folder/0128-longest-consecutive-sequence/solution.cpp
+++ my-folder/0128-longest-consecutive-sequence/solution.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
+        stack<int>st;
 
-        if(nums.empty()) return 0;
+        if( nums.size() == 0) 
+          return 0;
+        int curr = 0, maxx = 0;
 
         sort(nums.begin(), nums.end());
+        
 
-        int most = 1, curr = 1;
-
-        for (int i=1; i<n; i++)
-        {
-            if(nums[i] == nums[i-1] + 1) {
-                curr += 1;
-            } else if (nums[i] != nums[i-1])
-            {
-                most = max(curr, most);
-                curr = 1;
-            }
+        for(auto n: nums){
+          if(!st.empty() && n-1 == st.top()){
+            curr += 1;
+            st.pop();
+            st.push(n);
+            maxx = max(maxx, curr);
+          }
+          else if(!st.empty() && st.top() == n) 
+            continue;
+          else {
+            curr = 0;
+            st.push(n);
+          }
         }
-
-        return max(most, curr);
-
-
-
+        return maxx+1;
     }
-
-
 };
-
