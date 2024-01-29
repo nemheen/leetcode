f254b94a47be5486deeb220ed2659516299ae129
nemheen <40517906+nemheen@users.noreply.github.com>
2024-01-29 18:45:16 +0000

    [LeetCode Sync] Runtime - 590 ms (5.24%), Memory - 66.3 MB (95.47%)

--- my-folder/0176-second-highest-salary/solution.py
+++ my-folder/0176-second-highest-salary/solution.py
@@ -1,12 +1,12 @@
 import pandas as pd
+import numpy as np
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    salaries = employee['salary'].drop_duplicates()
-
-    second = salaries.nlargest(2).iloc[-1] if len(salaries) >= 2 else None
-    if second is None:
+    if employee['salary'].unique().shape[0] < 2:
         return pd.DataFrame({'SecondHighestSalary': [None]})
     else:
-       return pd.DataFrame({'SecondHighestSalary': [second]})
-
-
+        distinct_employee_salaries = employee['salary'].unique()
+        sorted_salaries = np.sort(distinct_employee_salaries)[::-1]
+        # sorted_employee_salary = employee['salary'].unique().sort_values(by='salary', ascending=False)
+        second_salary = sorted_salaries[1]
+        return pd.DataFrame({'SecondHighestSalary':[second_salary]})
