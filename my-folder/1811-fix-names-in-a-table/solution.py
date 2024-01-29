90cfaa337455ce42e3484cac82a4e223f1eff7c2
nemheen <40517906+nemheen@users.noreply.github.com>
2024-01-29 17:58:21 +0000

    [LeetCode Sync] Runtime - 796 ms (5.08%), Memory - 67.2 MB (93.75%)

---
+++ my-folder/1811-fix-names-in-a-table/solution.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users['name'] = users['name'].apply(lambda x: x.capitalize())
+    return users.sort_values(by='user_id', ascending=True)
+    
