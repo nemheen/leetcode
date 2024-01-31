c03abf31493f0334ef12901a1d7b95a035ae12e1
nemheen <40517906+nemheen@users.noreply.github.com>
2024-01-31 13:41:47 +0000

    [LeetCode Sync] Runtime - 501 ms (5.00%), Memory - 17.6 MB (15.07%)

---
+++ my-folder/0437-path-sum-iii/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pathSum(self, root, targetSum):
+        def dfs(node, targetSum):
+            if not node:
+                return 0
+
+            count = int(node.val == targetSum)
+            count += dfs(node.left, targetSum - node.val)
+            count += dfs(node.right, targetSum - node.val)
+
+            return count
+
+        if not root:
+            return 0
+
+        total_count = dfs(root, targetSum)
+        total_count += self.pathSum(root.left, targetSum)
+        total_count += self.pathSum(root.right, targetSum)
+
+        return total_count
