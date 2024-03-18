0b79344d334d0cb800263bc7d6fb4ef81f559c81
nemheen <40517906+nemheen@users.noreply.github.com>
2024-03-18 03:29:27 +0000

    [LeetCode Sync] Runtime - 69 ms (63.30%), Memory - 21.9 MB (95.87%)

---
+++ my-folder/1777-determine-if-two-strings-are-close/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int>f1(26, 0);
+        vector<int>f2(26, 0);
+
+        for (char ch : word1) {
+            f1[ch - 'a']++;
+        } 
+        for (char ch : word2) {
+            f2[ch - 'a']++;
+        }
+
+        for (int i=0; i<26; i++) {
+            if ((f1[i] == 0 && f2[i] != 0) ||  (f1[i] != 0 && f2[i] == 0)) return false;
+        }
+
+        sort(f1.begin(), f1.end());
+        sort(f2.begin(), f2.end());
+
+        for (int i=0; i<26; i++) {
+            if (f1[i] != f2[i]) return false;
+        }
+        return true;
+    }
+};
