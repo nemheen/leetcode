fa49cece1b0baf96df46468d5d921cf7c8c90941
nemheen <40517906+nemheen@users.noreply.github.com>
2024-03-21 00:15:59 +0000

    [LeetCode Sync] Runtime - 104 ms (85.25%), Memory - 19.1 MB (55.69%)

--- my-folder/0136-single-number/solution.py
+++ my-folder/0136-single-number/solution.py
@@ -1,7 +1,10 @@
 from collections import Counter
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-       a=Counter(nums)
-       res = list([k for k,v in a.items() if v ==1])
-       for i in res:
-           return i
+        count = Counter(nums)
+
+        for k, v in count.items():
+            if v == 1:
+                return k
+        
