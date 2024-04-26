575952ba1a40953a8063013c7eb3fe4f97df8148
nemheen <40517906+nemheen@users.noreply.github.com>
2024-04-26 14:53:08 +0000

    [LeetCode Sync] Runtime - 546 ms (5.01%), Memory - 64.5 MB (100.00%)

---
+++ my-folder/0620-not-boring-movies/solution.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    return cinema[(cinema['id']%2 != 0) & (~cinema['description'].str.contains('boring'))].sort_values(by='rating', ascending=False)
+    
