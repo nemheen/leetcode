590958419c700fd02995010beedcf66c6fe398f2
nemheen <40517906+nemheen@users.noreply.github.com>
2024-05-04 15:02:24 +0000

    [LeetCode Sync] Runtime - 35 ms (69.12%), Memory - 16.5 MB (58.74%)

--- my-folder/1013-fibonacci-number/solution.py
+++ my-folder/1013-fibonacci-number/solution.py
@@ -1,11 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        one,two, fib = 0, 1, 0
-        if n==0 or n==1:
+        if n <= 1:
             return n
-        for i in range(2, n+1):
-            fib = one + two
-            one = two
-            two = fib
-        return fib 
+        
+        memo = [0] * (n + 1)
+        memo[1] = 1
 
+        for i in range(2, n + 1):
+            memo[i] = memo[i - 1] + memo[i - 2]
+
+        return memo[n]
+        
