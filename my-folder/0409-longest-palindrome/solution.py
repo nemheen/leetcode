01bf983fad0ee674c300709eec5fb92279e0aa6b
nemheen <40517906+nemheen@users.noreply.github.com>
2024-06-04 00:51:30 +0000

    [LeetCode Sync] Runtime - 45 ms (7.42%), Memory - 16.5 MB (95.05%)

--- my-folder/0409-longest-palindrome/solution.py
+++ my-folder/0409-longest-palindrome/solution.py
@@ -1,18 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        res=0
-        counter = Counter(s)
-        y=0
-        for k,v in counter.items():
-            if v%2==0:
-                res += v
-                
+        ss = []
+        for letter in s:
+            if letter not in ss:
+                ss.append(letter)
             else:
-                res += v - 1
-                y=y+1
-     
-        if y>0:
-            return res+1
+                ss.remove(letter)
+        if len(ss) != 0:
+            return len(s) - len(ss) + 1
         else:
-            return res
-
+            return len(s)
+        
