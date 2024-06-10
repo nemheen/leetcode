121126555901edccc21ed30168b5dc537c755652
nemheen <40517906+nemheen@users.noreply.github.com>
2024-06-10 04:14:15 +0000

    [LeetCode Sync] Runtime - 472 ms (35.77%), Memory - 41.7 MB (5.39%)

--- my-folder/2428-equal-row-and-column-pairs/solution.py
+++ my-folder/2428-equal-row-and-column-pairs/solution.py
@@ -1,28 +1,25 @@
 import numpy as np
-from typing import List
 
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        grid = np.array(grid)
-        trans = grid.transpose()
+        hrow, hcol = {}, {}
+        n = len(grid)
+        k = 0
 
-        row_counts = {}
-        col_counts = {}
-
-        # Count occurrences of each row
         for row in grid:
-            row_tuple = tuple(row)
-            row_counts[row_tuple] = row_counts.get(row_tuple, 0) + 1
+            rowt = tuple(row)
+            hrow[rowt] = hrow.get(rowt, 0) + 1
 
-        # Count occurrences of each column
-        for col in trans:
-            col_tuple = tuple(col)
-            col_counts[col_tuple] = col_counts.get(col_tuple, 0) + 1
 
-        # Count pairs with equal row and column counts
-        equal_pairs = 0
-        for row_tuple, row_count in row_counts.items():
-            equal_pairs += row_count * col_counts.get(row_tuple, 0)
+        tgrid = np.transpose(grid)
 
-        return equal_pairs
+        for col in tgrid:
+            colt = tuple(col)
+            hcol[colt] = hcol.get(colt, 0) + 1
+
+        for t, c in hrow.items():
+            k += c * hcol.get(t, 0)
+
+        return k
+
 
