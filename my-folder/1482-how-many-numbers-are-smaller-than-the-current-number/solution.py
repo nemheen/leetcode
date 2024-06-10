a8cc9865145c39082864b8767e301ee1f4453d37
nemheen <40517906+nemheen@users.noreply.github.com>
2024-06-10 04:25:02 +0000

    [LeetCode Sync] Runtime - 208 ms (24.55%), Memory - 16.5 MB (68.00%)

--- my-folder/1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
+++ my-folder/1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
@@ -1,11 +1,15 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        num = sorted(nums)
-        mapping = {}
-        out = []
-        plus = 0
-        for i in range(len(num)):
-            if num[i] not in mapping:
-                mapping[num[i]] = i
+        n = len(nums)
+        out = [0] * n
+
+        for i in range(n):
+            if i == 0:
+                out[0] = sum(1 for n in nums[1:] if n < nums[0])
+            elif i == n-1:
+                out[n-1] = sum(1 for n in nums[:n] if n < nums[-1])
+            else:
+                out[i] = sum(1 for n in nums[:i] if n < nums[i]) + sum(1 for n in nums[i+1:] if n < nums[i])
+
+        return out
         
-        return [mapping[j] for j in nums]
