4e07f4fe036f11e5c2c2739b730272111ffe14d9
nemheen <40517906+nemheen@users.noreply.github.com>
2024-11-03 12:36:51 +0000

    [LeetCode Sync] Runtime - 3 ms (28.11%), Memory - 17.2 MB (8.65%)

--- my-folder/0871-keys-and-rooms/solution.py
+++ my-folder/0871-keys-and-rooms/solution.py
@@ -1,18 +1,27 @@
 class Solution:
+    def dfs(self, node, visited, graph):
+
+        if node not in visited:
+            visited.add(node)
+            for k in graph[node]:
+                self.dfs(k, visited, graph)
+
+
+    def buildG(self, rooms, n):
+        g = {}
+
+        for i, keys in enumerate(rooms):
+            g[i] = keys
+    
+        return g
+
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        
-        def dfs(rooms, node, visited):
+        n = len(rooms)
+        g = self.buildG(rooms, n)
+        vis = set()
 
-            if node not in visited:
-                visited.append(node)
+        self.dfs(0, vis, g)
+        return n==len(vis)
 
-                for neighbor in rooms[node]:
-                    dfs(rooms, neighbor, visited)
-            
-
-
-        visited = []
-        dfs(rooms, 0, visited)
-
-        return len(visited) == len(rooms)
+    
         
