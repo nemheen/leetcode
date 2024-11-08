b361249f2617221f58779bdba907586be223c4ea
nemheen <40517906+nemheen@users.noreply.github.com>
2024-11-08 02:05:16 +0000

    [LeetCode Sync] Runtime - 3 ms (94.88%), Memory - 16.9 MB (89.48%)

--- my-folder/0605-can-place-flowers/solution.py
+++ my-folder/0605-can-place-flowers/solution.py
@@ -1,24 +1,18 @@
-from typing import List
-
 class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        length = len(flowerbed)
-        if length == 1:
-            if flowerbed[0] == 0:
+    def canPlaceFlowers(self, f: List[int], n: int) -> bool:
+        i = 0
+        while i < len(f):
+            if n == 0:
                 return True
-            elif n == 0:
-                return True
-            else: return False
-   
-        if flowerbed[0] == 0 and flowerbed[1] == 0:
-            n -= 1
-            flowerbed[0] = 1
-        for i in range(1, length - 1):
-            if flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
-                n -= 1
-                flowerbed[i] = 1
-        if flowerbed[length - 1] == 0 and flowerbed[length - 2] == 0:
-            n -= 1
-            flowerbed[length - 1] = 1
-        return n <= 0
 
+            if f[i] == 0:
+                if (i==0 or f[i-1]==0) and (i==len(f)-1 or f[i+1]==0):
+                    f[i] == 1
+                    i += 2
+                    n -= 1
+                else:
+                    i += 1
+
+            else:
+                i += 1
+        return n==0
